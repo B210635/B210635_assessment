65384c097c45638c1632c5dc65eedc88daf6e291
B210635 <s1890318@ed.ac.uk>
2022-06-21 02:33:22 +0000

    New update on 'Notable line 10'.

--- RScripts/Using_R_in_Noteable.R
+++ RScripts/Using_R_in_Noteable.R
@@ -7,7 +7,7 @@
 
 #Here is an example of using R as a calculator to perform a numerical operation. 
 
-(55 + 7)^2 - 1*5/4
+(95 + 7)^2 - 1*5/4 
 
 
 # Using the c() and cbind() base R functions to combine data values and objects respectively. 
